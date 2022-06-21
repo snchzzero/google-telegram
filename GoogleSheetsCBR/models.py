ff0e2939f4001c6c962193683724c3f470f97ec7
snchzzero <aaevstafev@gmail.com>
2022-06-21 16:36:24 +0300

    delete models

--- GoogleSheetsCBR/models.py
+++ GoogleSheetsCBR/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Models(models.Model):
-    number_Model = models.IntegerField()
-    order_Model = models.IntegerField()
-    value_dolar_Model = models.IntegerField()
-    value_rub_Model = models.IntegerField()
-    delivery_time_Model = models.DateTimeField()
-
 class ModelSort(models.Model):
     sorts_Model = models.CharField(max_length=30, default="№")
     ASC_DESK_Model = models.CharField(max_length=30, default="возрастанию")
--- GoogleSheetsCBR/views.py
+++ GoogleSheetsCBR/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import psycopg2
 from google_sheets_cbr.config_db import host, user, password, db_name
 from google_sheets_cbr.script import db_google_sheets
 from datetime import datetime
 from .forms import Form_sort
-import os
-
 
 
 def home(request):
